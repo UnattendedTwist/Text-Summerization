955817733613b6cfde809dfee676143cedf5ef81
ArjunKR <104211493+UnattendedTwist@users.noreply.github.com>
2023-06-26 00:40:25 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function summarizeText() {
+    const inputText = document.getElementById('inputBox').value;
+
+    // TODO: Call your summarization program/function with the inputText
+
+    // Replace the placeholder text with the summarized result
+    document.getElementById('summaryBox').value = 'Summary will be displayed here';
+}
